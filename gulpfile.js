e3f82d00a3a6614a58f0e4d4955ee2585126a38b
Ian Mercadal <72464723+IanMercadal@users.noreply.github.com>
2021-08-25 16:13:20 +0200

    feat: Customizing project
    Writing the routes of the files for the automatization

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const {series, src, dest} = require('gulp');
+const {series, src, dest, watch} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 // Funcion que compila SASS
@@ -8,5 +8,16 @@ function css(){
         .pipe(sass())
         .pipe(dest('./build/css'))
 }
-
+function minificarcss(){
+    return src('src/scss/app.css')
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }))
+    .pipe(dest('./build/css'))
+}
+function watchArchivos(){
+    watch('src/scss/**/*.scss', css) // * = La carpeta actual - ** = Todos los archivos con esa extensión
+}
 exports.css = css;
+exports.minificarcss = minificarcss;
+exports.watchArchivos = watchArchivos;
